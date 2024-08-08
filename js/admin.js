00a835d5253f0cef2789f50ae15b20fb1d7fc6c1
Emilse Arias <emilsearias@gmail.com>
2024-08-08 16:34:08 -0300

    instanciamos a la ventana modal desde nuestro admin.js

--- js/admin.js
+++ js/admin.js
@@ -1,7 +1,16 @@
 import Contacto from "./classContacto.js";
-//aqui agrego la logica del CRUD
 
+//declaro las variables
 console.log('prueba desde admin.js')
 const contacto = new Contacto('Pereyra','Franco','franco@mail.com','381777888','-', '-')
 console.log(contacto);
+const modalContacto = new bootstrap.Modal(document.getElementById('modalAdminContacto'))
+const btnNuevo = document.getElementById('btnNuevo');
 
+//funciones
+const mostrarModal = ()=>{
+    modalContacto.show();
+}
+
+//aqui agrego la logica del CRUD
+btnNuevo.addEventListener('click', mostrarModal)
