b35364dae6527d4bae6539c115ad1352ec1eae29
Emilse Arias <emilsearias@gmail.com>
2024-08-13 16:03:25 -0300

    creamos un contacto y lo almacenamos en localstorage

--- js/admin.js
+++ js/admin.js
@@ -1,16 +1,49 @@
 import Contacto from "./classContacto.js";
 
 //declaro las variables
-console.log('prueba desde admin.js')
-const contacto = new Contacto('Pereyra','Franco','franco@mail.com','381777888','-', '-')
-console.log(contacto);
-const modalContacto = new bootstrap.Modal(document.getElementById('modalAdminContacto'))
-const btnNuevo = document.getElementById('btnNuevo');
+const modalContacto = new bootstrap.Modal(
+  document.getElementById("modalAdminContacto")
+);
+const btnNuevo = document.getElementById("btnNuevo");
+const formularioContacto = document.getElementById("formContacto");
+//traigo los input del formulario
+const apellido = document.getElementById("apellido");
+const nombre = document.getElementById("nombre");
+const email = document.getElementById("email");
+const foto = document.getElementById("foto");
+const github = document.getElementById("github"),
+ direccion = document.getElementById("direccion"),
+ telefono = document.getElementById("telefono");
+const listaContactos =[];
 
 //funciones
-const mostrarModal = ()=>{
-    modalContacto.show();
+const mostrarModal = () => {
+  modalContacto.show();
+};
+
+const crearContacto = (e) => {
+  e.preventDefault();
+  console.log("desde la funcion crear contacto");
+  //debo validar los datos del formulario
+  //crear el objeto
+  const nuevoContacto = new Contacto(apellido.value, nombre.value, email.value, telefono.value, github.value, direccion.value,foto.value );
+  console.log(nuevoContacto);
+  //quiero guardar el objeto en mi lista de contactos o array
+  listaContactos.push(nuevoContacto)
+  console.log(listaContactos)
+  limpiarFormulario();
+  //guardar el array en localstorage
+  guardarEnLocalstorage();
+};
+
+const limpiarFormulario = () =>{
+    formularioContacto.reset(); 
+}
+
+const guardarEnLocalstorage = ()=>{
+    localStorage.setItem('listaContactosKey', JSON.stringify(listaContactos));
 }
 
 //aqui agrego la logica del CRUD
-btnNuevo.addEventListener('click', mostrarModal)
+btnNuevo.addEventListener("click", mostrarModal);
+formularioContacto.addEventListener("submit", crearContacto);
--- js/classContacto.js
+++ js/classContacto.js
@@ -1,62 +1,85 @@
-export default class Contacto{
-    #id;
-    #apellido;
-    #nombre;
-    #email;
-    #telefono;
-    #github;
-    #direccion;
-    constructor(apellido, nombre, email, telefono, github, direccion){
-        this.#id = crypto.randomUUID();
-        this.#apellido = apellido;
-        this.#nombre = nombre;
-        this.#email = email;
-        this.#telefono = telefono;
-        this.#github = github;
-        this.#direccion = direccion;
-    }
+export default class Contacto {
+  #id;
+  #apellido;
+  #nombre;
+  #email;
+  #telefono;
+  #github;
+  #direccion;
+  #foto;
+  constructor(apellido, nombre, email, telefono, github, direccion, foto) {
+    this.#id = crypto.randomUUID();
+    this.#apellido = apellido;
+    this.#nombre = nombre;
+    this.#email = email;
+    this.#telefono = telefono;
+    this.#github = github;
+    this.#direccion = direccion;
+    this.#foto = foto;
+  }
 
-    //agregar los get y set
-    get id() {
-        return this.#id;
+  //agregar los get y set
+  get id() {
+    return this.#id;
+  }
+  set id(value) {
+    this.#id = value;
+  }
+  get apellido() {
+    return this.#apellido;
+  }
+  set apellido(value) {
+    this.#apellido = value;
+  }
+  get nombre() {
+    return this.#nombre;
+  }
+  set nombre(value) {
+    this.#nombre = value;
+  }
+  get email() {
+    return this.#email;
+  }
+  set email(value) {
+    this.#email = value;
+  }
+  get telefono() {
+    return this.#telefono;
+  }
+  set telefono(value) {
+    this.#telefono = value;
+  }
+  get github() {
+    return this.#github;
+  }
+  set github(value) {
+    this.#github = value;
+  }
+  get direccion() {
+    return this.#direccion;
+  }
+  set direccion(value) {
+    this.#direccion = value;
+  }
+  get foto() {
+    return this.#foto;
+  }
+
+  set foto(value) {
+    this.#foto = value;
+  }
+
+  //metodo para que funcione el JSON.stringify de js
+  toJSON(){
+    return {
+       id: this.id,
+       apellido: this.apellido,
+       nombre: this.nombre,
+       telefono: this.telefono,
+       foto: this.foto,
+       email: this.email,
+       direccion: this.direccion,
+       github: this.github
     }
-    set id(value) {
-        this.#id = value;
-    }
-    get apellido() {
-        return this.#apellido;
-    }
-    set apellido(value) {
-        this.#apellido = value;
-    }
-    get nombre() {
-        return this.#nombre;
-    }
-    set nombre(value) {
-        this.#nombre = value;
-    }
-    get email() {
-        return this.#email;
-    }
-    set email(value) {
-        this.#email = value;
-    }
-    get telefono() {
-        return this.#telefono;
-    }
-    set telefono(value) {
-        this.#telefono = value;
-    }
-    get github() {
-        return this.#github;
-    }
-    set github(value) {
-        this.#github = value;
-    }
-    get direccion() {
-        return this.#direccion;
-    }
-    set direccion(value) {
-        this.#direccion = value;
-    }
+  }
 }
